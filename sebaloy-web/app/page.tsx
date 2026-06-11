aab4a90f4b5db18c20b1e7a97efe150ba8bb0dea
didc2026 <didc2026@gmail.com>
2026-06-11 01:46:56 +0000

    UI update

--- sebaloy-web/app/page.tsx
+++ sebaloy-web/app/page.tsx
@@ -101,8 +101,8 @@ const placeOrder = async () => {
   return (
     <main className="min-h-screen bg-slate-100 p-8">
       <div className="max-w-7xl mx-auto">
-        <h1 className="text-4xl font-bold mb-2 text-teal-600">
-          Sebaloy
+<h1 className="text-4xl font-bold mb-2 text-red-600">      
+    Sebaloy
         </h1>
 
         <p className="text-gray-600 mb-6">
